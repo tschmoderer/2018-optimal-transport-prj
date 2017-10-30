e90f939f89a4d72f3d3f773889b3ace39deeefc0
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2017-10-30 17:18:34 +0100

    minor delete

--- brain_47_optimal/gradient_col.m
+++
@@ -1,9 +0,0 @@
-function gradI_col=gradient_col(I,dim)
-
-h=[-1,0,1;-2,0,2;-1,0,1];
-gradI_col=conv2(I,h,'same');
-column1=I(:,2)-I(:,1);
-columndim=I(:,dim(2))-I(:,dim(2)-1);
-
-gradI_col(:,1)=column1;
-gradI_col(:,dim(2))=columndim;
--- brain_47_optimal/norme_grad.m
+++
@@ -1,8 +0,0 @@
-function normecarree_I=norme_grad(I,dim);
-
-gradI_col=gradient_col(I,dim);
-%figure;imagesc(gradI_col),colormap(gray);
-
-gradI_lig=gradient_lig(I,dim);
-%figure;imagesc(gradI_lig),colormap(gray);
-normecarree_I=gradI_col.^2+gradI_lig.^2;
