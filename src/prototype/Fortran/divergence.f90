9232669a3d580ec1aa7c4bc04c7cce85f4e78224
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2017-10-31 00:27:33 +0100

    final version

--- src/prototype/Fortran/divergence.f90
+++ src/prototype/Fortran/divergence.f90
@@ -7,6 +7,6 @@ subroutine divergence(mbar,fbar,d,N,Q)
     double precision, dimension(N+2,Q+1), intent(in) :: mbar;
     double precision, dimension(N+1,Q+2), intent(in) :: fbar;
     double precision, dimension(N+1,Q+1), intent(out) :: d; 
-
+    
     d = mbar(2:N+2,:) - mbar(1:N+1,:) + fbar(:,2:Q+2) - fbar(:,1:Q+1);
 end subroutine
