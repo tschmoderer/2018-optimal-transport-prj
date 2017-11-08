9269afa10dcb63b741c762bbe7c7a6c2a6717a08
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2017-11-08 21:44:53 +0100

    wrk in prgss

--- src/prototype/RaZ/boundary.m
+++
@@ -1,4 +0,0 @@
-function b = boundary(mbar,fbar)
-    b.m = [mbar(1,:);mbar(end,:)];
-    b.f = [fbar(:,1),fbar(:,end)];
-end 
--- src/prototype/RaZ/interpolation.m
+++
@@ -1,4 +0,0 @@
-function [m,f] = interpolation(mbar,fbar)
-    m = (mbar(1:end-1,:) + mbar(2:end,:))/2;
-    f = (fbar(:,1:end-1) + fbar(:,2:end))/2;
-end
