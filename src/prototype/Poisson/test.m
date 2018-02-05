3804bac1345df664209ce697c7a67c3b233b3bf9
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2018-02-05 19:02:24 +0100

    wrking on FFT

--- src/prototype/Poisson/test.m
+++ src/prototype/Poisson/test.m
@@ -2,8 +2,8 @@ clear all
 close all
 clc
 %% test FFT
-N = 5;
-Q = 5;
+N = 21;
+Q = 19;
 
 X       = (0:N)/N; T = (0:Q)/Q;
 [XX,YY] = meshgrid(X,T); YY = flipud(YY);
@@ -17,41 +17,43 @@ dt  = @(f) Q*(f([2:end end],:) - f); % dérivation selon ---> t
 dxS = @(dm) N*[-dm(:,1) , dm(:,1:end-2) - dm(:,2:end-1) , dm(:,end-1)];
 dtS = @(df) Q*[-df(1,:) ; df(1:end-2,:) - df(2:end-1,:) ; df(end-1,:)];
 
-A  = @(w)  [-dxS(w(:,:,1)) + dt(w(:,:,2)); w(end,:,2) ;w(1,:,2)];
+A  = @(w)  [-dxS(w(:,:,1)) + dt(w(:,:,2)); w(end,:,2); w(1,:,2)];
 AS = @(Aw) cat(3,-dx(Aw(1:Q+1,:)),dtS(Aw(1:Q+1,:)) + [Aw(Q+3,:);zeros(Q-1,N+1);Aw(Q+2,:)]);
 
 y = [zeros(Q+1,N+1);f0;f1];
 
 w = rand(Q+1,N+1,2);
-pC = w + AS(poisson(y - A(w),zeros(1,Q+1),zeros(1,Q+1),f1,f0,N,Q,1e-5))
+
+b =  - (-dxS(w(:,:,1)) + dt(w(:,:,2)));
+
+u = zeros(Q+1,N+1); 
+u(1,:) = f1 - w(1,:,2); u(end,:) = f0 - w(end,:,2);
+u(:,1) = - w(:,1,1); u(:,end) = - w(:,end,1);
+
+dz = 1/N; dt = 1/Q;
+S = 0.5*(1/dz^2 + 1/dt^2)^-1;
+ukp1 = u; 
+err = 1; tol = 1e-5;
+k = 0;
+while (err > tol) 
+%    ukp1(2:end-1,2:end-1) = S*(d(2:end-1,2:end-1)+ (u(2:end-1,3:end) + u(2:end-1,1:end-2))/dt^2 + (u(3:end,2:end-1) + u(1:end-2,2:end-1))/dx^2);
+ 		for i = 2:Q
+ 			for j = 2:N
+ 				ukp1(i,j) = S*(b(i,j) + (u(i,j+1) + u(i,j-1))/dt^2 + (u(i+1,j) + u(i-1,j))/dz^2);
+ 			end
+ 		end
+    err = norm(ukp1-u,inf);
+    u = ukp1;
+    k = k +1;
+end
+
+return 
+
+pC = w + cat(3,-dx(u),dtS(u))
+
+err = @(w) norm(A(w)-y)/norm(y);
+
+fprintf('Error before projection: %.2e\n', err(w));
+fprintf('Error after projection: %.2e\n', err(pC));
 
 
-
-% f = -dxS(w(:,:,1)) + dt(w(:,:,2));
-% hx                    = 1/(N+1);
-% dn                    = 0:1:N;
-% depn                  = 2*cos(pi*dn/(N+1))-2;
-% 
-% hy                    = 1/(Q+1);
-% dm                    = 0:1:Q;
-% depm                  = 2*cos(pi*dm/(Q+1))-2;
-% 
-% denom                = repmat(depn(:)/hx^2,1,Q+1) + repmat(depm(:)'/hy^2,N+1,1);
-% denom(denom(:)==0)  = 1;
-% 
-% 
-% fhat                  = dct2(f);
-% uhat                  = -(fhat)./denom';
-% 
-% res = idct2(uhat);
-
-% pC = w0 + cat(3,-dx(res),dtS(res));
-% 
-% %% check div=0
-% y = [zeros(Q+1,N+1);f0;f1];
-% err = @(w) norm(A(w)-y)/norm(y);
-% 
-% % fprintf('Error before projection: %.2e\n', err(w));
-% % fprintf('Error after projection: %.2e\n', err(pC));
-% 
-% pC
