622947d53b8916f7361b1a8331ee9b32cc5288ca
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2018-04-29 17:21:12 +0200

    Cleanup

--- src/prototype/code/toolbox/@staggered/proj_plus.m
+++
@@ -1,4 +0,0 @@
-function A = proj_plus(A)
-
-
-A.M{end}(:)       = max(A.M{end}(:),0);
--- src/prototype/code/toolbox/pd_append.m
+++
@@ -1,5 +0,0 @@
-function Y = pd_append(Y,u,v)
-
-% aux function for PD algorithm
-
-Y(:,:,1:2, end+1) = cat(3, u, v );
